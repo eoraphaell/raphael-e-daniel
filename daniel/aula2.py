5b62446ec99abf0b24a0687a3ce021257acfa2ff
eoraphaell <132069125+eoraphaell@users.noreply.github.com>
2023-10-04 11:36:05 -0300

    Add files via upload

---
+++ daniel/aula2.py
@@ -0,0 +1,11 @@
+soma = 1+1
+multiplicacao = 4*4
+divisao = 30/3
+potencia + 7***3
+
+print('soma = ', soma)
+print('multiplicação', multiplicacao)
+print('divisao',sivisao)
+print('potencia',potencial)
+
+ v  
---
+++ daniel/aula3.py
@@ -0,0 +1,5 @@
+idade = int(input("informe a sua idade: ")
+            if idade >= 18:
+            print9"pode curtir a balada!)
+else:
+print("você foi barrado!")
